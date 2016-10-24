e0734295a2c211d2805dca96718cb521213605c4
Quentin Donnellan <quentin@spideroak.com>
2016-10-24 09:50:23 -0500

    Run the non-blocking bot before the flask-app.

--- src/app.py
+++ src/app.py
@@ -30,5 +30,5 @@ def pull_request_webhook(payload, guid):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
     bot.run(block=False)
+    app.run(debug=True, host='0.0.0.0', port=8080)
